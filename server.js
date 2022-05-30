eb5ef4f34d0b1ba2498b2e2de815ff408b003964
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-05-30 16:46:17 -0500

    Added some comments

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 require("dotenv");
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// ROUTES
 app.get("/", (req, res) => {
     console.log("root url");
     res.send("Hello Word!");
 });
 
+// LISTEN
 app.listen(3000, () => {
     console.log("listening on 3000");
 });
