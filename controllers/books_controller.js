d8106047fd59787f20dac66ebd7eb2d3e4e73252
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-05-31 22:41:00 -0500

    Added the dummy db file and get routes

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db");
 
-//LIST ONE
+// LIST ONE
 router.get("/info/:id", (req, res) => {
     console.log("books info id");
-    res.send(`In books/info/${req.params.id}`);
+    let book = db.filter((book) => book.id == req.params.id);
+    // let book = db[req.params.id];
+    res.json(book);
 });
 
 // LIST ALL
 router.get("/", (req, res) => {
     console.log("books root");
-    res.send("In books/");
+    res.send(db);
 });
 
 // CREATE ONE
@@ -22,10 +25,12 @@ router.post("/", (req, res) => {
 // DELETE ONE
 router.delete("/delete/:id", (req, res) => {
     console.log("books delete");
+    // db.splice(req.params.id - 1, req.params.id);
     res.send(`In books/delete/${req.params.id}`);
+    // res.json(db);
 });
 
-// UUPDATE ONE
+// UPDATE ONE
 router.patch("/update/:id", (req, res) => {
     console.log("books update");
     res.send(`In books/update/${req.params.id}`);
